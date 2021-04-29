d837abccd457279c4a822b52faa53d0b1fde736f
anisha17github <79098831+anisha17github@users.noreply.github.com>
2021-04-29 16:49:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var girl, boy;
+var snowflakes;
+var snowflakeImg, girlImg, boyImg, backImg;
+
+function setup() {
+  createCanvas(1000,500);
+ 
+}
+
+function draw() {
+  background("pink");  
+  
+  drawSprites();
+}
